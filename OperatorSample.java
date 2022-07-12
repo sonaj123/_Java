8af1a14517b60e45f7fcee7c3e2c12c02fb6c984
sonaj123 <108450315+sonaj123@users.noreply.github.com>
2022-07-12 11:53:11 +0530

    Add files via upload

---
+++ OperatorSample.java
@@ -0,0 +1,16 @@
+class OperatorSample
+{
+	public static void main(String[] args)
+	{
+		int a=10,b=3;
+		int c;
+		c=a+b;
+		System.out.println("a+b="+c);
+		c=a-b;
+		System.out.println("a-b="+c);
+		c=a*b;
+		System.out.println("a*b="+c);
+		c=a/b;
+		System.out.println("a/b="+c);
+	}
+}
